57c785cb4cc0381214a90e665ef5bea11db2d038
yurixtugal <yurixtugal12@gmail.com>
2023-09-22 19:49:01 -0500

    Add icons to add, edit and delete channels and manage members in server list of channels and members

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -1,56 +1,89 @@
 import { ChannelType, MemberRole } from "@prisma/client";
-import { Hash, Headphones, Plus, Settings, ShieldAlert, ShieldCheck, Video } from "lucide-react";
+import {
+  Delete,
+  Edit,
+  Hash,
+  Headphones,
+  Plus,
+  Settings,
+  ShieldAlert,
+  ShieldCheck,
+  Trash,
+  Video,
+} from "lucide-react";
+import { Avatar, AvatarImage } from "../ui/avatar";
 
 const mapTitle = {
-    [ChannelType.TEXT]: "TEXT CHANNELS",
-    [ChannelType.AUDIO]: "AUDIO CHANNELS",
-    [ChannelType.VIDEO]: "VIDEO CHANNELS",
-    [MemberRole.ADMIN]: "MEMBERS",
-    [MemberRole.MODERATOR]: "MEMBERS",
-    [MemberRole.GUEST]: "MEMBERS"
-}
+  [ChannelType.TEXT]: "TEXT CHANNELS",
+  [ChannelType.AUDIO]: "AUDIO CHANNELS",
+  [ChannelType.VIDEO]: "VIDEO CHANNELS",
+  [MemberRole.ADMIN]: "MEMBERS",
+  [MemberRole.MODERATOR]: "MEMBERS",
+  [MemberRole.GUEST]: "MEMBERS",
+};
 
 const iconMapGroups = {
-    [ChannelType.TEXT]: <Hash className="mr-2 h-4 w-4" />,
-    [ChannelType.AUDIO]: <Headphones className="mr-2 h-4 w-4" />,
-    [ChannelType.VIDEO]: <Video className="mr-2 h-4 w-4" />,
-    [MemberRole.ADMIN]: <ShieldAlert className="mr-2 h-4 w-4 text-rose-500" />,
-    [MemberRole.MODERATOR]: <ShieldCheck className="mr-2 h-4 w-4 text-indigo-500" />,
-    [MemberRole.GUEST]: null
-  }
+  [ChannelType.TEXT]: <Hash className="mr-2 h-4 w-4" />,
+  [ChannelType.AUDIO]: <Headphones className="mr-2 h-4 w-4" />,
+  [ChannelType.VIDEO]: <Video className="mr-2 h-4 w-4" />,
+  [MemberRole.ADMIN]: <ShieldAlert className="mr-2 h-4 w-4 text-rose-500" />,
+  [MemberRole.MODERATOR]: (
+    <ShieldCheck className="mr-2 h-4 w-4 text-indigo-500" />
+  ),
+  [MemberRole.GUEST]: null,
+};
 
 interface serverSectionProps {
-    data:{
-        type: ChannelType | MemberRole,
-        detail:{
-            label: string,
-        }[]|undefined
-    }
+  data: {
+    type: ChannelType | MemberRole;
+    detail:
+      | {
+          label: string;
+          imageLink?: string;
+        }[]
+      | undefined;
+  };
 }
 
 const ServerSection = ({ data }: serverSectionProps) => {
-    return (
-      <>
-        <div className="font-semibold text-sm text-zinc-500/80 m-2 flex gap-x-2 justify-between">
-          {mapTitle[data.type]}
-          {data.type === ChannelType.TEXT ||
-          data.type === ChannelType.AUDIO ||
-          data.type === ChannelType.VIDEO ? (
-            <Plus className="h-4 w-4 text-zinc-500/80" />
-          ) : (
-            <Settings className="h-4 w-4 text-zinc-500/80" />
-          )}
-        </div>
-        <div className="my-2 mx-4 font-semibold text-sm text-zinc-400">
-          {data.detail?.map((detail) => (
-            <div key={detail.label} className="flex items-center my-2">
-              {iconMapGroups[data.type]}
-              <span>{detail.label}</span>
+  return (
+    <>
+      <div className="  font-semibold text-sm text-zinc-500/80 m-2 flex gap-x-2 justify-between">
+        {mapTitle[data.type]}
+        {data.type === ChannelType.TEXT ||
+        data.type === ChannelType.AUDIO ||
+        data.type === ChannelType.VIDEO ? (
+          <button>
+            <Plus className=" h-4 w-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+          </button>
+        ) : (
+          <button>
+            <Settings className=" h-4 w-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+          </button>
+        )}
+      </div>
+      <div className="my-4 mx-4 font-semibold text-sm text-zinc-400">
+        {data.detail?.map((detail) => (
+          <button
+            key={detail.label}
+            className="group  rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1"
+          >
+            {iconMapGroups[data.type]}
+            {!!detail.imageLink && (
+              <Avatar className="h-8 w-8 md:h-8 md:w-8 mr-2">
+                <AvatarImage src={detail.imageLink} />
+              </Avatar>
+            )}
+            <span>{detail.label}</span>
+            <div className="ml-auto flex">
+              <Edit className="mr-1 hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+              <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
             </div>
-          ))}
-        </div>
-      </>
-    );
-}
- 
+          </button>
+        ))}
+      </div>
+    </>
+  );
+};
+
 export default ServerSection;
--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,4 +1,4 @@
-import { getCurrentProfile } from "@/lib/current-profile"
+import { getCurrentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { Channel, ChannelType, Member, MemberRole } from "@prisma/client";
 import { redirect } from "next/navigation";
@@ -11,36 +11,44 @@ import { Separator } from "@/components/ui/separator";
 import ServerSection from "./server-section";
 
 interface serverSidebarProps {
-    serverId: string
-}    
+  serverId: string;
+}
 
 interface interfaceToSend {
   type: ChannelType | MemberRole;
-  detail: {
-    label: string;
-  }[]|undefined;
+  detail:
+    | {
+        label: string;
+      }[]
+    | undefined;
 }
 
-const prepareDataToSection = (lstSource: Channel[] | Member[] | undefined, sourceType: ChannelType | MemberRole ): interfaceToSend  => {
-  
+const prepareDataToSection = (
+  lstSource: Channel[] | Member[] | undefined,
+  sourceType: ChannelType | MemberRole
+): interfaceToSend => {
+  const isMember =
+    sourceType === MemberRole.ADMIN ||
+    sourceType === MemberRole.MODERATOR ||
+    sourceType === MemberRole.GUEST;
   const sourceDetail = lstSource?.map((source) => {
-   if (source as Channel) {
-    return {
-      label: (source as Channel).name,
+    if (!isMember) {
+      return {
+        label: (source as Channel).name,
+      };
+    } else {
+      return {
+        label: (source as memberWithProfile).profile.name,
+        imageLink: (source as memberWithProfile).profile.imageUrl,
+      };
     }
-   } else{
-    return {
-      label: (source as memberWithProfile).profile.name,
-    }
-   }
-    
-  })
-  
+  });
+
   return {
-      type: sourceType,
-      detail: sourceDetail
-  }
-}
+    type: sourceType,
+    detail: sourceDetail,
+  };
+};
 
 const prepareDataToSearch = (
   textChannels: Channel[] | undefined,
@@ -65,97 +73,115 @@ const prepareDataToSearch = (
         id: channel.id,
       })),
     },
-    { label: "Video Channels",
-    detail: videoChannels?.map((channel) => ({
-      label: channel.name,
-      type: channel.type,
-      id: channel.id,
-    })), },
-    { label: "Members",
-    detail: members?.map((member) => ({
+    {
+      label: "Video Channels",
+      detail: videoChannels?.map((channel) => ({
+        label: channel.name,
+        type: channel.type,
+        id: channel.id,
+      })),
+    },
+    {
+      label: "Members",
+      detail: members?.map((member) => ({
         label: member.profile.name,
         type: member.role,
         id: member.profile.id,
-        })),
+      })),
     },
   ];
 };
 
-export const ServerSidebar = async ({
-    serverId
-}: serverSidebarProps) => {
+export const ServerSidebar = async ({ serverId }: serverSidebarProps) => {
+  const profile = await getCurrentProfile();
 
-    const profile = await getCurrentProfile();
+  if (!profile) {
+    return redirect("/");
+  }
 
-    if (!profile) {
-        return redirect("/")
-    }
-
-    const server = await db.server.findUnique({
-        where: { id: serverId },
+  const server = await db.server.findUnique({
+    where: { id: serverId },
+    include: {
+      channels: {
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+      members: {
         include: {
-            channels:{
-                orderBy: {
-                    createdAt: "asc"
-                }
-            },
-            members: {
-                include: {
-                    profile: true
-                },
-                orderBy: {
-                    role: "asc"
-                }    
-            }    
-        }
-    
-    })
-    
-    const textChannels = server?.channels.filter(channel => channel.type === ChannelType.TEXT);
-    const audioChannels = server?.channels.filter(channel => channel.type === ChannelType.AUDIO);
-    const videoChannels = server?.channels.filter(channel => channel.type === ChannelType.VIDEO);
+          profile: true,
+        },
+        orderBy: {
+          role: "asc",
+        },
+      },
+    },
+  });
 
-    const members = server?.members.filter(member => member.profileId !== profile.id)
+  const textChannels = server?.channels.filter(
+    (channel) => channel.type === ChannelType.TEXT
+  );
+  const audioChannels = server?.channels.filter(
+    (channel) => channel.type === ChannelType.AUDIO
+  );
+  const videoChannels = server?.channels.filter(
+    (channel) => channel.type === ChannelType.VIDEO
+  );
 
-    const dataToSearch = prepareDataToSearch(textChannels,audioChannels,videoChannels,members);
+  const members = server?.members.filter(
+    (member) => member.profileId !== profile.id
+  );
 
-    const textChannelsToSend = prepareDataToSection(textChannels, ChannelType.TEXT)
-    const audioChannelsToSend = prepareDataToSection(audioChannels, ChannelType.AUDIO)
-    const videoChannelsToSend = prepareDataToSection(videoChannels, ChannelType.VIDEO)
-    const membersToSend = prepareDataToSection(members, MemberRole.GUEST)
+  const dataToSearch = prepareDataToSearch(
+    textChannels,
+    audioChannels,
+    videoChannels,
+    members
+  );
 
-    if (!server) {
-        return redirect("/")
-    }
+  const textChannelsToSend = prepareDataToSection(
+    textChannels,
+    ChannelType.TEXT
+  );
+  const audioChannelsToSend = prepareDataToSection(
+    audioChannels,
+    ChannelType.AUDIO
+  );
+  const videoChannelsToSend = prepareDataToSection(
+    videoChannels,
+    ChannelType.VIDEO
+  );
+  const membersToSend = prepareDataToSection(members, MemberRole.GUEST);
+  console.log(members);
 
-    const role = server.members.find(member => member.profileId === profile.id)?.role   
- 
-    return (
-      <div className="h-full w-full text-primary flex flex-col dark:bg-[#2B2D31] bg-[#F2F3F5]">
-        <ServerHeader server={server} role={role} />
-        <ScrollArea className="flex-1 px-3">
-          <div className="mt-2">
-            <ServerSearch data={dataToSearch}/>
-          </div>
-          <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 w-full rounded-md mx-auto my-4"/>
-          {!!textChannels?.length && (
-            <ServerSection data={textChannelsToSend} />
-          )  }
+  if (!server) {
+    return redirect("/");
+  }
 
-          {!!audioChannels?.length && (
-            <ServerSection data={audioChannelsToSend} />
-          )  }
+  const role = server.members.find(
+    (member) => member.profileId === profile.id
+  )?.role;
 
-          {!!videoChannels?.length && (
-            <ServerSection data={videoChannelsToSend} />
-          )  }
+  return (
+    <div className="h-full w-full text-primary flex flex-col dark:bg-[#2B2D31] bg-[#F2F3F5]">
+      <ServerHeader server={server} role={role} />
+      <ScrollArea className="flex-1 px-3">
+        <div className="mt-2">
+          <ServerSearch data={dataToSearch} />
+        </div>
+        <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 w-full rounded-md mx-auto my-4" />
+        {!!textChannels?.length && <ServerSection data={textChannelsToSend} />}
 
-          {!!members?.length && (
-            <ServerSection data={membersToSend} />
-          )  }
-        </ScrollArea>
-        
-      </div>
-    );
-}
+        {!!audioChannels?.length && (
+          <ServerSection data={audioChannelsToSend} />
+        )}
 
+        {!!videoChannels?.length && (
+          <ServerSection data={videoChannelsToSend} />
+        )}
+
+        {!!members?.length && <ServerSection data={membersToSend} />}
+      </ScrollArea>
+    </div>
+  );
+};
